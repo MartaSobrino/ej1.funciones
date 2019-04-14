4f76328070af1d2dc6967b5ec8bcba36f62daaef
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-04-14 13:05:37 +0200

    done

--- js/main.js
+++ js/main.js
@@ -4,16 +4,12 @@ function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
 
-const randomAguacate = getRandomNumber(10);
-const randomPatata = getRandomNumber(10);
-const randomPizza = getRandomNumber(10);
-
 function ten(word, number){
     for(let i=0; i<number; i++){
        console.log(word);
     }
 }
 
-ten('aguacate', randomAguacate);
-ten('patata', randomPatata);
-ten('pizza', randomPizza);
+ten('aguacate', getRandomNumber(10));
+ten('patata', getRandomNumber(10));
+ten('pizza', getRandomNumber(10));
